66f79209dfa126c7831f9a98eecec1be1c93a93c
shuto kikai <yysskikai04@icloud.com>
2023-06-24 18:55:34 +0900

    second-commit

--- hash.rb
+++ hash.rb
@@ -1,5 +1,6 @@
 players_count = {baseball: 9, soccer: 11, basketball: 5}
 puts players_count[:soccer]
+puts players_count[:baseball]
 
 
 dic_members = {ceo: "noro", ai_teacher: "nakao", rails_teacher: "miyashita"}
