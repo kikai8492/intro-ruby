e1e8ea8054b6d046d1c20c9b7695956fabb926c3
shuto kikai <yysskikai04@icloud.com>
2023-06-24 18:52:31 +0900

    hash

--- hash.rb
+++ hash.rb
@@ -1,5 +1,6 @@
 players_count = {baseball: 9, soccer: 11, basketball: 5}
 puts players_count[:soccer]
 
+
 dic_members = {ceo: "noro", ai_teacher: "nakao", rails_teacher: "miyashita"}
 puts dic_members[:ceo]
