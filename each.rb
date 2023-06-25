544efbdebfe85ca70e10e04c04aeecd3067460c1
shuto kikai <yysskikai04@icloud.com>
2023-06-25 20:39:44 +0900

    each.rbのコード削除

--- each.rb
+++ each.rb
@@ -7,11 +7,7 @@ members.each do |subject,teacher|
 end
 
 
-members = [{japanese: "田中"}, {math: "佐藤"}, {science: "鈴木"}, {english: "山田"}]
 
-members.each do |subject, teacher|
-  puts "教科: #{subject}, 教師: #{teacher}"
-end
 
 
 select = ["以下より行う操作を選んでください","1:ブログを作成する","2:作成されたブログを見る","3:ブログアプリを終了する"]
