1174cbd712790b9160bb1f596257c130b54a7847
shuto kikai <yysskikai04@icloud.com>
2023-06-25 12:08:33 +0900

    行の削除

--- each.rb
+++ each.rb
@@ -1,13 +1,12 @@
-
-
 members = {japanese: "田中",math: "佐藤", science: "鈴木", english: "山田"}
-
 
 members.each do |subject,teacher|
 
   puts "・教科:#{subject} 教師:#{teacher}"
 
 end
+
+
 
 
 members = [{japanese: "田中"}, {math: "佐藤"}, {science: "鈴木"}, {english: "山田"}]
